c9c889b28b1e137cbdd4a74bc2decef3713e1cac
lzh247 <92080423+lzh247@users.noreply.github.com>
2022-01-24 09:35:31 +0100

    Code red to green

--- 1DL471/index.php
+++ 1DL471/index.php
@@ -6,7 +6,7 @@
 <p>My first paragraph.</p>
 <a href="https://www.w3schools.com">This is a link</a>
 <img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
-<p style="color:red;">This is a red paragraph.</p>
+<p style="color:green;">This is a green paragraph.</p>
 <p title="I'm a tooltip">This is a paragraph.</p>
 
 </body>
