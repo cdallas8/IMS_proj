bd026b32f7e6416875b2c35f75dcfa2fd0d4f1c2
lzh247 <92080423+lzh247@users.noreply.github.com>
2022-01-24 08:59:25 +0100

    Upload practice php, in htdoc file

---
+++ 1DL471/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+<a href="https://www.w3schools.com">This is a link</a>
+<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
+<p style="color:red;">This is a red paragraph.</p>
+<p title="I'm a tooltip">This is a paragraph.</p>
+
+</body>
+</html>
