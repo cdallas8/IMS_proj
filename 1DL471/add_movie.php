f90be949d68dfeaeca7c7b19d8c47cb7145ad5a9
lzh247 <92080423+lzh247@users.noreply.github.com>
2022-01-26 11:27:56 +0100

    Update add_movie.php

--- 1DL471/add_movie.php
+++ 1DL471/add_movie.php
@@ -1,8 +1,13 @@
 
 <?php
+
+include "connect.php";
 $mname = $_POST["movie name"]; 
 $myear = $_POST["year"]; 
 print("You entered $mname and $myear")
 
-
+// you can put the received data into the database here
+// NULL for automation (auto increment)
+$sql = "INSERT INTO movies (mid, mname, myear, mgenreid, mrating) VALUES (NULL, '$mname', '$myear') ";
+$result = $mysqli->query($sql);
 ?>
