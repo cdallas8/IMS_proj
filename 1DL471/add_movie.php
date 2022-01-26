91ec769c62994647a5b061613d949fb46536baa4
cdallas8 <92080785+cdallas8@users.noreply.github.com>
2022-01-26 09:03:54 +0100

    Create add_movie.php

---
+++ 1DL471/add_movie.php
@@ -0,0 +1,12 @@
+
+<?php
+$
+book_name = $_ book_name "];
+$
+publish_year = $_ publish_year
+print("You entered $
+book_name and $ publish_year
+// you can put the received data into the
+database
+here
+?>
