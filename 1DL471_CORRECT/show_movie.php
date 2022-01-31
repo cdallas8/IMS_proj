9be500b0e8dba7d3fe5a38c60d19c59c0f4b3344
cdallas8 <92080785+cdallas8@users.noreply.github.com>
2022-01-31 13:19:31 +0100

    Update show_movie.php

--- 1DL471_CORRECT/show_movie.php
+++ 1DL471_CORRECT/show_movie.php
@@ -7,7 +7,10 @@ include "connect.php";
 
 //$search=$_POST['search'];
 $result = mysqli_query ($link, "SELECT m.mname,m.myear,g.mgenre,m.mrating FROM movies as m LEFT JOIN genres as g ON m.mgenreid=g.gid WHERE m.mname LIKE '%" . $_POST['search'] . "%'");
+// With LEFT JOIN we have all the movies table + matching records from the genre tables - in this way it is possible to get the genre. 
+// '%" . $_POST['search'] . "%' we used this syntax because it was not working with the variable $search
 if(mysqli_num_rows($result)==0) {
+// returns the number of rows in a result set - so if it is 0 then there is nothing in the result variable. 
     print("No results matching your query<br>\n");
 } else {
     echo "<table border='1'>";
@@ -18,7 +21,7 @@ if(mysqli_num_rows($result)==0) {
     echo "<tr><th>Movie name</th><th>Published year</th><th>Genre</th><th>Rating</th></tr>";
     while($row = mysqli_fetch_row($result)){
         echo "<tr><td>";
-        echo $row[0];
+        echo $row[0]; // from each row we get the first element 
         echo "</td><td>";
         echo $row[1];
         echo "</td><td>";
